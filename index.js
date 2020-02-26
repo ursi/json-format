2464d67c63dfbaaaf7dd83c9053e01662614cb19
Mason Mackaman <masondeanm@aol.com>
2020-02-26 12:09:33 -0500

    Preserve trailing NL

--- index.js
+++ index.js
@@ -8,13 +8,18 @@ const [jsonPath, indentation = 4] = process.argv.slice(2);
 
 const truePath = path.join(...ensureExtension(jsonPath).split(`/`));
 
+const jsonText = fs.readFileSync(truePath, 'utf-8');
+
+const trailingNL = jsonText.slice(-1) === `\n`;
+
 fs.writeFileSync
 	( truePath
 	, JSON.stringify
-		( JSON.parse(fs.readFileSync(truePath))
+		( JSON.parse(jsonText)
 		, null
 		, +indentation
 		)
+	    + (trailingNL ? `\n` : ``)
 	);
 
 function ensureExtension(path) {
