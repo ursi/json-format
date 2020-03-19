5dd6f4715b1841b2d137519f373775c8a2370a6c
Mason Mackaman <masondeanm@aol.com>
2020-03-18 20:58:30 -0400

    Add help

--- index.js
+++ index.js
@@ -2,28 +2,33 @@
 
 const
 	fs = require(`fs`),
-	path = require(`path`);
+	path = require(`path`),
+	args = process.argv.slice(2);
 
-const [jsonPath, indentation = 4] = process.argv.slice(2);
+if (args.length === 0 || args[0] === `--help` || args[0] === `-h`) {
+	console.log(`json-format <json path minus extension>[.json] [0-10]`);
+} else {
+	const [jsonPath, indentation = 4] = process.argv.slice(2);
 
-const truePath = path.join(...ensureExtension(jsonPath).split(`/`));
+	const truePath = path.join(...ensureExtension(jsonPath).split(`/`));
 
-const jsonText = fs.readFileSync(truePath, 'utf-8');
+	const jsonText = fs.readFileSync(truePath, 'utf-8');
 
-const trailingNL = jsonText.slice(-1) === `\n`;
+	const trailingNL = jsonText.slice(-1) === `\n`;
 
-fs.writeFileSync
-	( truePath
-	, JSON.stringify
-		( JSON.parse(jsonText)
-		, null
-		, +indentation
-		)
-	    + (trailingNL ? `\n` : ``)
-	);
+	fs.writeFileSync
+		( truePath
+		, JSON.stringify
+			( JSON.parse(jsonText)
+			, null
+			, +indentation
+			)
+			+ (trailingNL ? `\n` : ``)
+		);
 
-function ensureExtension(path) {
-	return /\.json$/.test(path)
-		? path
-		: path + `.json`;
+	function ensureExtension(path) {
+		return /\.json$/.test(path)
+			? path
+			: path + `.json`;
+	}
 }
